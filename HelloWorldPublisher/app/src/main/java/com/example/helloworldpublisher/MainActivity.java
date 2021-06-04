f60babe498f28650508020d2ee8ea3042e30d437
pattaraporn punya <pattaraporn.fibo@mail.kmutt.ac.th>
2021-06-04 17:13:55 +0700

    first commit

---
+++ HelloWorldPublisher/app/src/main/java/com/example/helloworldpublisher/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworldpublisher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] args = {};
+        HelloWorldPublisher.main(args);
+    }
+}
